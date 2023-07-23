4a2f0d14f765946645c7426b052545e1fd03aa50
Lucas Chagas <L.Candemil@edu.pucrs.br>
2023-07-23 20:39:39 -0300

    pp

--- .py/aprovadoReprovado.py
+++ .py/aprovadoReprovado.py
@@ -1,36 +1,39 @@
-#INSERCAO DA NOTAS
-nota1 = float(input('Informe a primeira nota: '))
-if nota1 > 10 or nota1 < 0: 
-    nota1 = float(input('Informe a primeira nota: '))
-    
-nota2 = float(input('Informe a segunda nota: '))
-if nota2 > 10 or nota2 < 0: 
-    nota2 = float(input('Informe a segunda nota: '))
-    
-nota3 = float(input('Informe a terceira nota: '))
-if nota3 > 10 or nota3 < 0: 
-    nota3 = float(input('Informe a terceira nota: '))
-    
-nota4 = float(input('Informe a quarta nota: '))
-if nota4 > 10 or nota4 < 0: 
-    nota4 = float(input('Informe a quarta nota: '))
-    
-nota5 = float(input('Informe a quinta nota: '))
-if nota5 > 10 or nota5 < 0: 
-    nota5 = float(input('Informe a quinta nota: '))
+notas = []
+cont = 0
+checagem = True
+#FUNCAO PARA INSERCAO DA NOTAS
+def def_notas():
+    nota = ' '
+    if not checagem:
+        nota = input('Informe a nota da G2: ')
+    else:
+        nota = input(f'Informe sua {cont+1}a nota: ') 
+    while not nota.isnumeric():
+        nota = input('Valor informado nao eh um numero positivo! Digite novamente: ')
+    while float(nota) > 10:
+        nota = input('Nota infomada nao esta entre 0 e 10! Digite novamente: ')
+        while not nota.isnumeric():
+            nota = input('Valor informado nao eh um numero positivo! Digite novamente: ')  
+    return float(nota)
 
-total =(nota1+nota2+nota3+nota4+nota5)/5
+t = 5
+while t:
+    notas.append(def_notas())
+    cont+=1
+    t-=1
 
-def checar_aprovacao(nota1, nota2, nota3, nota4, nota5):
+total = (notas[0]+notas[1]+notas[2]+notas[3]+notas[4])/5
+
+def checar_aprovacao():
     return total >= 7
 
 #CASO SEJA TRUE ESTA APROVADO E SE FOR FALSE ESTA REPROVADO
-checagem = checar_aprovacao(nota1, nota2, nota3, nota4, nota5)
+checagem = checar_aprovacao()
 
 if checagem:
     print('APROVADO')
 else:
-    notaG2 = float(input('Informe a nota da G2: '))
+    notaG2 = def_notas()
     if (notaG2+total)/2 >= 5:
         print('APROVADO')
     else:
