f94f786c5f19bf664fd0d3226ba8f9e76966a2af
Lucas Chagas <L.Candemil@edu.pucrs.br>
2023-07-23 18:55:44 -0300

    as

--- .py/aprovadoReprovado.py
+++ .py/aprovadoReprovado.py
@@ -1,30 +1,23 @@
 #INSERCAO DA NOTAS
-nota1 = input('Informe a primeira nota: ')
-if float(nota1) > 10 or float(nota1) < 0: 
-    nota1 = input('Informe a primeira nota: ')
+nota1 = float(input('Informe a primeira nota: '))
+if nota1 > 10 or nota1 < 0: 
+    nota1 = float(input('Informe a primeira nota: '))
     
-nota2 = input('Informe a segunda nota: ')
-if float(nota2) > 10 or float(nota2) < 0: 
-    nota2 = input('Informe a segunda nota: ')
+nota2 = float(input('Informe a segunda nota: '))
+if nota2 > 10 or nota2 < 0: 
+    nota2 = float(input('Informe a segunda nota: '))
     
-nota3 = input('Informe a terceira nota: ')
-if float(nota3) > 10 or float(nota3) < 0: 
-    nota3 = input('Informe a terceira nota: ')
+nota3 = float(input('Informe a terceira nota: '))
+if nota3 > 10 or nota3 < 0: 
+    nota3 = float(input('Informe a terceira nota: '))
     
-nota4 = input('Informe a quarta nota: ')
-if float(nota4) > 10 or float(nota4) < 0: 
-    nota4 = input('Informe a quarta nota: ')
+nota4 = float(input('Informe a quarta nota: '))
+if nota4 > 10 or nota4 < 0: 
+    nota4 = float(input('Informe a quarta nota: '))
     
-nota5 = input('Informe a quinta nota: ')
-if float(nota5) > 10 or float(nota5) < 0: 
-    nota5 = input('Informe a quinta nota: ')
-
-#CONVERSAO PARA INTEIRO    
-nota1 = float(nota1)
-nota2 = float(nota2)
-nota3 = float(nota3)
-nota4 = float(nota4)
-nota5 = float(nota5)
+nota5 = float(input('Informe a quinta nota: '))
+if nota5 > 10 or nota5 < 0: 
+    nota5 = float(input('Informe a quinta nota: '))
 
 total =(nota1+nota2+nota3+nota4+nota5)/5
 
@@ -37,8 +30,7 @@ checagem = checar_aprovacao(nota1, nota2, nota3, nota4, nota5)
 if checagem:
     print('APROVADO')
 else:
-    notaG2 = input('Informe a nota da G2: ')
-    notaG2 = float(notaG2)
+    notaG2 = float(input('Informe a nota da G2: '))
     if (notaG2+total)/2 >= 5:
         print('APROVADO')
     else:
