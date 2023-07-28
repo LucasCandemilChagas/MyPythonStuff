5d9520e378b35b7c4901772446d2c65ff816cff3
Lucas Chagas <L.Candemil@edu.pucrs.br>
2023-07-28 11:04:43 -0300

    pp

---
+++ .py/WarGame/cards.py
@@ -0,0 +1,14 @@
+class Card:
+    def __init__(self, suit, rank):
+         self.suit = suit
+         self.rank = rank
+    
+    def __str__(self):
+        return self.rank + ' of ' + self.suit      
+     
+
+two_hearts = Card('Hearts', 'Two')
+
+
+
+          
