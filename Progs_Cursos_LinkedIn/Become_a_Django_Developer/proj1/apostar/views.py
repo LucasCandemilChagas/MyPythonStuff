63dfb1f2cc8d82c845f1ca044d96ac31281f60cb
LucasCandemilChagas <l.candemil@edu.pucrs.br>
2025-04-01 12:24:20 -0300

    proporcao tela

--- Progs_Cursos_LinkedIn/Become_a_Django_Developer/proj1/apostar/views.py
+++ Progs_Cursos_LinkedIn/Become_a_Django_Developer/proj1/apostar/views.py
@@ -9,13 +9,17 @@ def aposta(request):
         filled_form = ApostaForm(request.POST)
         nota = ''
         if filled_form.is_valid():
-            filled_form.save()
+            created_form = filled_form.save()
+            created_form_pk = created_form.id
             nota = (f"Aposta {filled_form.cleaned_data['numero1']} "
                     f"{filled_form.cleaned_data['numero2']} "
                     f"{filled_form.cleaned_data['numero3']} "
                     f"{filled_form.cleaned_data['numero4']} "
                     f"{filled_form.cleaned_data['numero5']} enviada com sucesso!")
-        return render(request, 'aposta/aposta.html', {'apostaform':filled_form, 'nota':nota, 'multiplas_apostasform':filled_multform})
+        else:
+            created_form_pk = None
+            nota = 'Aposta Invalida! Tente Novamente.'         
+        return render(request, 'aposta/aposta.html', {'created_form_pk':created_form_pk,'apostaform':filled_form, 'nota':nota, 'multiplas_apostasform':filled_multform})
     else:
         form = ApostaForm()
     return render(request, 'aposta/aposta.html', {'apostaform':form, 'multiplas_apostasform':filled_multform})
--- Progs_Cursos_LinkedIn/Become_a_Django_Developer/proj1/smartnotes/settings.py
+++ Progs_Cursos_LinkedIn/Become_a_Django_Developer/proj1/smartnotes/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'notas',
     'usuario',
     'apostar',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
